9f98f7800e3f9ec6146b17d9a47f87b87fe1cc78
bestradag05 <bestradag05@gmail.com>
2024-06-11 14:57:01 -0500

    refactor (categories): use ListCrudRepository

--- src/main/java/com/iroman/restaurantmanagement/persistence/repository/CategoryRepository.java
+++ src/main/java/com/iroman/restaurantmanagement/persistence/repository/CategoryRepository.java
@@ -1,8 +1,8 @@
 package com.iroman.restaurantmanagement.persistence.repository;
 
 import com.iroman.restaurantmanagement.persistence.entity.Category;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.ListCrudRepository;
 
-public interface CategoryRepository extends CrudRepository<Category, Long> {
+public interface CategoryRepository extends ListCrudRepository<Category, Long> {
 
 }
